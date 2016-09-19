1e89ddf66012bf6f2e003cc3360751b67714c2dc
Neil Dunn <n768d465@ku.edu>
2016-09-18 19:21:10 -0500

    Added documentation to the php files

--- eventRemover.php
+++ eventRemover.php
@@ -1,9 +1,12 @@
 <?php
+/**
+    A date and index is received from "eventHandler.js". This file opens up and clears "eventInformation.txt" and scans all indeces of the events. If the index of the event in this file does NOT match up with the event sent from the JS file, the line gets rewritten onto "eventInformation.txt".  Otherwise, the line is not written on the text file.
+*/
 
-$file = "eventInformation.txt";
-$lines = file($file);
-date_default_timezone_set("America/Chicago");
-//$newOutFile = "eventDeleter.txt";
+/** The text file to be opend */
+$file = "eventInformation.txt"; 
+
+$lines = file($file); // 
 
 $dateLook = $_POST['dateLookup'];
 $dateID = substr($dateLook, 0,8);
@@ -13,8 +16,6 @@ $inFile = fopen($file, "w+");
 //$newInFile = fopen($newOutFile, "a+");
 //$j = 0;
 $lineToRemove = "";
-
-echo "Your events for " . $dateLook . " are: " . "<br>";
 
 for($i = 0; $i < sizeof($lines); $i++)
 {
