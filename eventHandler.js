6ab2fcd6da419cbe32c56c25c7ee8f806a702d09
JackSt96 <jackstevens@ku.edu>
2016-10-05 12:08:09 -0500

    Add files via upload

--- eventHandler.js
+++ eventHandler.js
@@ -3,24 +3,58 @@ This function take the queryString and validates that it is in the correct
 MMDDYYYY format.Then it asks the user to inout the event for the day.
 Then it calls the php script to place the event in eventInformaion.txt
 */
-function addEvent(queryString) {
+function addEvent() {
     var event = {
         date: "no date",
-        description: "no description"
+        end_date: "",
+        description: "no description",
+        start_time: "no time",
+        end_time: "no time",
+        type: "single"
     };
+    var x = document.getElementById("event_date");
+    var text = x.value.toString().substring(5,7) + x.value.toString().substring(8,10) + x.value.toString().substring(0,4);
 
-    event.date = queryString;
-
-    if (queryString.length != 8) {
-
-        alert("Please select date.");
-    } else {
-        var descriptionPromptInput = prompt("What is your event?");
-        event.description = descriptionPromptInput;
+    var end = document.getElementById("end_date").value;
+    if(end == "")
+    {
+      alert("empty");
+    }
+    else {
+      event.end_date = end;
+    }
+    event.start_time = document.getElementById("start_time").value;
+    event.end_time = document.getElementById("end_time").value;
+    if(event.start_time == ""){
+      event.start_time = "00:00"
+    }
+    if(event.end_time == ""){
+      event.end_time = "24:00"
     }
 
+    if (document.getElementById("mult").checked)
+    {
+      event.type = "mult";
+    }
+    if (document.getElementById("rec").checked)
+    {
+      event.type = "rec";
+    }
+
+    alert(event.type);
+    var queryString = text;
+
+
+
+    event.date = queryString;
+    var descriptionPromptInput = document.getElementById("event_name");
+
+    event.description = descriptionPromptInput.value;
+
+
+
     if (descriptionPromptInput == "") {
-        alert("Please type in an event.")
+        alert("Please type in an event.");
     } else {
         $.post("eventFile.php", event);
         readEvent(queryString);
